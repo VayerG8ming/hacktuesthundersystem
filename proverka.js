dd5c233b7b5f0ff5ff79ea237a9c285b9829931a
marti456 <37450663+marti456@users.noreply.github.com>
2018-03-16 18:12:58 +0200

    Add files via upload

---
+++ proverka.js
@@ -0,0 +1,30 @@
+var points=0;
+function onRadioPress1(btn) {
+  console.log("Class: " + btn.className);
+  points=points+1
+}
+
+function onRadioPress2(btn) {
+  console.log("Class: " + btn.className);
+  points=points+1
+}
+
+function onRadioPress3(btn) {
+  console.log("Class: " + btn.className);
+  points=points+1
+}
+
+function onRadioPress4(btn) {
+  console.log("Class: " + btn.className);
+  points=points+2
+}
+
+function onRadioPress5(btn) {
+  console.log("Class: " + btn.className);
+  points=points+2
+}
+
+function onRadioPress6(btn) {
+  console.log("Class: " + btn.className);
+  points=points+3
+}
